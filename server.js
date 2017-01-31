7fa290e87a01983a1144818d5df5e5c13524f7dd
Christ <smaashthemac@gmail.com>
2017-01-31 14:25:02 -0600

    changed port

--- server.js
+++ server.js
@@ -77,6 +77,6 @@ app.get("/random", function(req, res) {
 // });
 
 // set the app to listen on port 3000
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
 	console.log("App running on port 3000");
 });
